afbe9c6d73a8387d6c9e26781149ecced5f0f232
ANA PAULA ROCHA  PASSOS <aanapaularm@gmail.com>
2020-10-13 16:50:56 -0700

    add question 232

---
+++ 232-ImplementQueueUsingStacks.py
@@ -0,0 +1,32 @@
+import collections
+class MyQueue:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = collections.deque([], 100)
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        return self.queue.popleft()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return len(self.queue) == 0
+
