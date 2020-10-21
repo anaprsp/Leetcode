70a0552f088d4c3f102e30e2c7ad522a4b5ab83f
ANA PAULA ROCHA  PASSOS <aanapaularm@gmail.com>
2020-10-21 12:53:44 -0700

    add a true solution

--- 232-ImplementQueueUsingStacks.py
+++ 232-ImplementQueueUsingStacks.py
@@ -1,32 +1,74 @@
+# import collections
+# class MyQueue:
+#     def __init__(self):
+#         """
+#         Initialize your data structure here.
+#         """
+#         self.queue = collections.deque([], 100)
+
+#     def push(self, x: int) -> None:
+#         """
+#         Push element x to the back of queue.
+#         """
+#         self.queue.append(x)
+
+#     def pop(self) -> int:
+#         """
+#         Removes the element from in front of queue and returns that element.
+#         """
+#         return self.queue.popleft()
+
+#     def peek(self) -> int:
+#         """
+#         Get the front element.
+#         """
+#         return self.queue[0]
+
+#     def empty(self) -> bool:
+#         """
+#         Returns whether the queue is empty.
+#         """
+#         return len(self.queue) == 0
+
 import collections
 class MyQueue:
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.queue = collections.deque([], 100)
+        self.inStack = []
+        self.outStack = []
 
     def push(self, x: int) -> None:
         """
         Push element x to the back of queue.
         """
-        self.queue.append(x)
+        self.inStack.append(x)
 
     def pop(self) -> int:
         """
         Removes the element from in front of queue and returns that element.
         """
-        return self.queue.popleft()
+        if not self.outStack and self.inStack:
+            for i in range(len(self.inStack)):
+                self.outStack.append(self.inStack.pop())
+            return self.outStack.pop()
+        else:
+            return self.outStack.pop()
+
 
     def peek(self) -> int:
         """
         Get the front element.
         """
-        return self.queue[0]
+        if self.outStack:
+            return self.outStack[-1]
+
+        if not self.outStack and self.inStack:
+            return self.inStack[0]
 
     def empty(self) -> bool:
         """
         Returns whether the queue is empty.
         """
-        return len(self.queue) == 0
-
+        return not len(self.outStack) and  not len(self.inStack)
